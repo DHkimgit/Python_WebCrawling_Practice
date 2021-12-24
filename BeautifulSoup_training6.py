54b76e35341f0c37297959b1715047ede6fa9e6d
DHkimgit <96455522+DHkimgit@users.noreply.github.com>
2021-12-24 22:18:32 +0900

    Add files via upload

---
+++ BeautifulSoup_training6.py
@@ -0,0 +1,28 @@
+import bs4
+
+html_str = """
+<html>
+    <body>
+        <ul class="ko">
+            <li>
+                <a herf="https://www.naver.com/">네이버</a>
+            </li>
+            <li>
+                <a herf="https://www.daum.net/">다음</a>
+            </li>
+        </ul>
+        <ul class="sns">
+            <li>
+                <a herf="https://www.google.com/">구글</a>
+            </li>
+            <li>
+                <a herf="https://www.facebook.com/">페이스북</a>
+            </li>
+        </ul>
+    </body>
+</html>
+"""
+
+bs_obj = bs4.BeautifulSoup(html_str, "html.parser")
+atag = bs_obj.find("a")
+print(atag['herf'])
