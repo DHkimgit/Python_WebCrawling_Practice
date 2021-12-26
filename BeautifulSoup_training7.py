0658dfa8b64f29bb0152e59d8b82c5b2e0388fc8
DHkimgit <96455522+DHkimgit@users.noreply.github.com>
2021-12-26 23:05:13 +0900

    Add files via upload

---
+++ BeautifulSoup_training7.py
@@ -0,0 +1,11 @@
+import urllib.request
+import bs4
+
+url = "http://www.naver.com/"
+html = urllib.request.urlopen(url)
+
+bs_obj = bs4.BeautifulSoup(html, "html.parser")
+
+top_right = bs_obj.find("div", {"class":"area_links"})
+
+print(top_right)
